78f06b16bae0a001c54e60120a66b3a798aa6a6a
Beah <beah.dev@gmail.com>
2020-10-04 22:07:34 -0400

    Crud Colaboradores, filtrando pela empresa logada

--- apps/colaboradores/views.py
+++ apps/colaboradores/views.py
@@ -4,3 +4,7 @@ from .models import Colaborador
 
 class ColaboradoresList(ListView):
     model = Colaborador
+
+    def get_queryset(self):
+        empresa_logada = self.request.user.colaborador.empresa
+        return Colaborador.objects.filter(empresa=empresa_logada)
