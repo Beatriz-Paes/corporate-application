f58157058c103d341f05a38b8e0ffc8ff6eb99a8
Beah <beah.dev@gmail.com>
2020-10-04 22:38:53 -0400

    Crud colaboradores, Update

--- apps/colaboradores/urls.py
+++ apps/colaboradores/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ColaboradoresList
+from .views import ColaboradoresList, FuncionarioEdit
 
 urlpatterns = [
     path('', ColaboradoresList.as_view(), name='list_colaboradores'),
+    path('editar/<int:pk>/', FuncionarioEdit.as_view(), name='update_colaborador'),
 
 ]
--- apps/colaboradores/views.py
+++ apps/colaboradores/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, UpdateView
 from .models import Colaborador
 
 
@@ -8,3 +8,8 @@ class ColaboradoresList(ListView):
     def get_queryset(self):
         empresa_logada = self.request.user.colaborador.empresa
         return Colaborador.objects.filter(empresa=empresa_logada)
+
+
+class FuncionarioEdit(UpdateView):
+    model = Colaborador
+    fields = ['nome', 'departamentos']
