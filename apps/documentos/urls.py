d361dd945b7061d068d1178b390f00b331211f4f
Beah <beah.dev@gmail.com>
2020-12-10 16:44:43 -0400

    Crud Documentos #2

--- apps/documentos/urls.py
+++ apps/documentos/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import DocumentoCreate, DocumentoDelete
+from .views import DocumentoCreate
 
 urlpatterns = [
-    path('novo', DocumentoCreate.as_view(), name='create_document'),
-    path('deletar/<int:pk>/', DocumentoDelete.as_view(), name='delete_documento'),
+    path('novo/<int:colaborador_id>', DocumentoCreate.as_view(), name='create_document'),
+    # path('deletar/<int:pk>/', DocumentoDelete.as_view(), name='delete_documento'),
 
 ]
--- apps/documentos/views.py
+++ apps/documentos/views.py
@@ -8,7 +8,16 @@ class DocumentoCreate(CreateView):
     model = Documento
     fields = ['descricao', 'arquivo']
 
+    def post(self, request, *args, **kwargs):
+        form = self.get_form()
+        form.instance.pertence_id = self.kwargs['colaborador_id']
 
-class DocumentoDelete(DeleteView):
-    model = Documento
-    success_url = reverse_lazy('list_documents')
+        if form.is_valid():
+            return self.form_valid(form)
+        else:
+            return self.form_invalid(form)
+
+
+# class DocumentoDelete(DeleteView):
+#     model = Documento
+#     success_url = reverse_lazy('list_documents')
