ff212db62617ffe28fa35990bea27dfe5b393982
Beah <beah.dev@gmail.com>
2020-12-10 23:57:25 -0400

    Listagem do banco de horas

--- apps/registro_hora_extra/models.py
+++ apps/registro_hora_extra/models.py
@@ -5,6 +5,7 @@ from apps.colaboradores.models import Colaborador
 class RegistroHoraExtra(models.Model):
     motivo = models.CharField(max_length=100)
     colaborador = models.ForeignKey(Colaborador, on_delete=models.PROTECT)
+    horas = models.DecimalField(max_digits=5, decimal_places=2)
 
     def __str__(self):
         return self.motivo
--- apps/registro_hora_extra/views.py
+++ apps/registro_hora_extra/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from apps.registro_hora_extra.models import RegistroHoraExtra
+
+
+class HoraExtraList(ListView):
+    model = RegistroHoraExtra
+
+    def get_queryset(self):
+        empresa_logada = self.request.user.colaborador.empresa
+        return RegistroHoraExtra.objects.filter(colaborador__empresa=empresa_logada)
