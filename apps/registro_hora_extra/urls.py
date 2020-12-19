ed5c3caaea3eb3bc6396c84a9a008226fb40ba43
Beah <beah.dev@gmail.com>
2020-12-19 19:26:33 -0400

    Diferentes redirecionamentos ao salvar hora extra

--- apps/registro_hora_extra/urls.py
+++ apps/registro_hora_extra/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import HoraExtraList, HoraExtraCreate, HoraExtraEdit, HoraExtraDelete
+from .views import HoraExtraList, HoraExtraCreate, HoraExtraEdit, HoraExtraDelete, HoraExtraEditBase
 
 urlpatterns = [
     path('', HoraExtraList.as_view(), name='list_hora_extra'),
     path('novo', HoraExtraCreate.as_view(), name='create_hora_extra'),
-    path('editar/<int:pk>/', HoraExtraEdit.as_view(), name='update_hora_extra'),
+    path('editar-funcionario/<int:pk>/', HoraExtraEdit.as_view(), name='update_hora_extra'),
+    path('editar/<int:pk>/', HoraExtraEditBase.as_view(), name='update_hora_extra_base'),
     path('deletar/<int:pk>/', HoraExtraDelete.as_view(), name='delete_hora_extra'),
 
 ]
