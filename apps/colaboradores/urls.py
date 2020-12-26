b38594457e899671909b4668ab560bea8df493d1
Beah <beah.dev@gmail.com>
2020-12-26 14:14:10 -0400

    Relatório de colaboradores em pdf com reportlab

--- apps/colaboradores/urls.py
+++ apps/colaboradores/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import ColaboradoresList, ColaboradorEdit, ColaboradorDelete, ColaboradorCreate
+from .views import ColaboradoresList, ColaboradorEdit, ColaboradorDelete, ColaboradorCreate, relatorio_colaboradores
 
 urlpatterns = [
     path('', ColaboradoresList.as_view(), name='list_colaboradores'),
     path('novo/', ColaboradorCreate.as_view(), name='create_colaborador'),
     path('editar/<int:pk>/', ColaboradorEdit.as_view(), name='update_colaborador'),
     path('deletar/<int:pk>/', ColaboradorDelete.as_view(), name='delete_colaborador'),
+    path('relatorio-colaboradores', relatorio_colaboradores, name='relatorio_colaboradores'),
 
 ]
--- apps/colaboradores/views.py
+++ apps/colaboradores/views.py
@@ -1,3 +1,7 @@
+import io
+from django.http import FileResponse, HttpResponse
+from reportlab.pdfgen import canvas
+
 from django.contrib.auth.models import User
 from django.views.generic import ListView, UpdateView, DeleteView, CreateView
 from .models import Colaborador
@@ -34,3 +38,33 @@ class ColaboradorCreate(CreateView):
         colaborador.save()
 
         return super(ColaboradorCreate, self).form_valid(form)
+
+
+def relatorio_colaboradores(request):
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = 'attachment; filename="mypdf.pdf"'
+
+    buffer = io.BytesIO()
+
+    p = canvas.Canvas(buffer)
+    p.drawString(250, 810, "Lista de Colaboradores")
+
+    # palavras = ['palavra1', 'palavra2', 'palavra3']
+    colaboradores = Colaborador.objects.filter(empresa=request.user.colaborador.empresa)
+
+    str_ = 'Nome: %s | Hora Extra: %sh'
+
+    y = 750
+
+    for c in colaboradores:
+        p.drawString(10, y - 20, str_ % (c.nome, c.total_horas_extra))
+        y -= 20
+
+    p.showPage()
+    p.save()
+
+    pdf = buffer.getvalue()
+    buffer.close()
+    response.write(pdf)
+
+    return response
